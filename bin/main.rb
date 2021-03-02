8545b5692df1d23edf71deb021b817b77b948df4
mahtsham <muhammadahtsham357@gmail.com>
2021-03-03 01:27:23 +0500

    add basic user interface

--- bin/main.rb
+++ bin/main.rb
@@ -1,2 +1,16 @@
 #!/usr/bin/env ruby
-puts "Hello World!"
+puts 'Welcome to Tic Tac Toe game'
+puts 'enter payer one name : '
+player_one = gets.chomp
+puts 'enter payer two name : '
+player_two = gets.chomp
+puts "#{player_one} will play with X and #{player_two} will play 0"
+puts "let's start . . ."
+
+puts '+---+---+---+'
+puts '| 1 | 2 | 3 |'
+puts '+---+---+---+'
+puts '| 4 | 5 | 6 |'
+puts '+---+---+---+'
+puts '| 7 | 8 | 9 |'
+puts '+---+---+---+'
