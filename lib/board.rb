b6ae195b86557278cc323daa7d0648d56ac273b0
mahtsham <muhammadahtsham357@gmail.com>
2021-03-10 14:19:04 +0500

    add changes

--- lib/board.rb
+++ lib/board.rb
@@ -27,12 +27,6 @@ class Game
     @board[number - 1] == number
   end
 
-  def winner?
-    WIN_COMBINATION.any? do |combo|
-      [@board[combo[0]], @board[combo[1]], @board[combo[2]]].uniq.length == 1
-    end
-  end
-
   def full?
     @board.all? { |x| %w[X O].include?(x) }
   end
