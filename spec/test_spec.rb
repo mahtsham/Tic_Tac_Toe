f9cb77949bc19979e3dbe56ccf5d21fb25b62d5d
mahtsham <muhammadahtsham357@gmail.com>
2021-03-25 23:03:33 +0500

    add negative test for add

--- spec/test_spec.rb
+++ spec/test_spec.rb
@@ -13,6 +13,17 @@ describe Board do
       expect(board.update(1, 'O')).to eql('O')
     end
   end
+  describe 'update' do
+    it 'update the move' do
+        expect(board.update(1, 'O')).to eql('O')
+        expect(board.update(1, false)).to eql(false)
+    end
+  end
+  describe 'update' do
+    it 'update the move' do
+      expect(board.update(2, false)).to eql(false)
+    end
+  end
   describe 'winner?' do
     it 'checks the winner' do
       expect(board.winner?).to eql(false)
